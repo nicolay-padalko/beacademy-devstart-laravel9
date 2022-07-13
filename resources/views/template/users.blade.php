3d828e4d6a54de6af59f4307d626eedfe6cd9ede
Nicolay Padalko <nicolay.padalko@gmail.com>
2022-07-12 21:25:21 -0300

    autenticacao

--- resources/views/template/users.blade.php
+++ resources/views/template/users.blade.php
@@ -1,18 +1,16 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-<div class="container w-70 p-3">
+<div class="container w-75 p-3">
     <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
@@ -24,9 +22,40 @@
                 </li>
             </ul>
         </div>
+        <div>
+            <div class="container">
+                <div class="row">
+                    <div>
+                        <ul class="navbar-nav mr-auto">
+                            @if(Auth::user())
+                                <li class="nav-item">
+                                    <a class="nav-link text-white" href="#">{{ Auth::user()->name }}</a>
+                                </li>
+                                <li class="nav-item">
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+                                        <x-responsive-nav-link class="nav-link text-white" :href="route('logout')"
+                                                               onclick="event.preventDefault();
+                                                                this.closest('form').submit();">
+                                            {{ __('Sair') }}
+                                        </x-responsive-nav-link>
+                                    </form>
+                                </li>
+                            @else
+                                <li class="nav-item">
+                                    <a class="nav-link text-white" href="{{ route('login') }}">Entrar</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-white" href="{{ route('register') }}">Registrar-se</a>
+                                </li>
+                            @endif
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </nav>
     @yield('body')
 </div>
-
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,29 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{PostController, UserController, ViaCepController};
 
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/users/{id}/posts', [PostController::class, 'show'])->name('posts.show');
+require __DIR__.'/auth.php';
 
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/user', [UserController::class, 'store'])->name('users.store');
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/user', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+    Route::get('/users/{id}/posts', [PostController::class, 'show'])->name('posts.show');
+});
 
 
-//Route::get('/viacep', [ViaCepController::class, 'index'])->name('viacep.index');
-//Route::post('/viacep', [ViaCepController::class, 'index'])->name('viacep.index.post');
-//Route::get('/viacep/{cep}', [ViaCepController::class, 'show'])->name('viacep.index');
+
 
 
